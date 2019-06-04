d52179c8d607d40a633b8ea2fc621ecc3f77a24e
Alberto <albertomaronezio@hotmai.com>
2019-06-04 13:30:39 -0300

    CaixaEletronico

---
+++ CaixaEletronico/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEletronico
+{
+    class Cliente
+    {
+        public string Nome { get; set; }
+
+        public string Rg { get; set; }
+
+        public string Endereco { get; set; }
+
+        public Cliente(string nome)
+        {
+            this.Nome = nome;
+        }
+    }
+}
