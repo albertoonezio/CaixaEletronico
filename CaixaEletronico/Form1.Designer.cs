378d14ba1d82e06abd3e244f5310a400cff63422
Alberto <albertomaronezio@hotmai.com>
2019-06-04 16:31:01 -0300

    CaixaEletronico

--- CaixaEletronico/Form1.Designer.cs
+++ CaixaEletronico/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.txtNomeTitular = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -41,21 +42,32 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // txtNomeTitular
+            // 
+            this.txtNomeTitular.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNomeTitular.Location = new System.Drawing.Point(249, 175);
+            this.txtNomeTitular.Name = "txtNomeTitular";
+            this.txtNomeTitular.Size = new System.Drawing.Size(342, 26);
+            this.txtNomeTitular.TabIndex = 1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtNomeTitular);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtNomeTitular;
     }
 }
 
